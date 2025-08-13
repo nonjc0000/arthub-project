c2029e6db7908e4c6ee1e2c04678dc76002d09ae
nonjc0000 <nonjc0000@gmail.com>
2025-08-13 16:44:16 +0800

    0813

--- src/ForDisplay.jsx
+++ src/ForDisplay.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
 import './css/reset.css'
 import './css/ForDisplay.css'
-import NavBar from './components/NavBar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Find_type from './pages/Find_type'
-import Find_map from './pages/Find_map'
-import Find_schedule from './pages/Find_schedule'
-import Blog from './pages/Blog'
-import About from './pages/About'
-import User from './pages/User'
-import Footer from './components/Footer'
-import Hamgurger from './components/Hamgurger'
+
+import Button from './components/Button'
 
 const ForDisplay = () => {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/Find_type' element={<Find_type />}></Route>
-                <Route path='/Find_map' element={<Find_map />}></Route>
-                <Route path='/Find_schedule' element={<Find_schedule />}></Route>
-                <Route path='/Blog' element={<Blog />}></Route>
-                <Route path='/About' element={<About />}></Route>
-                <Route path='/User' element={<User />}></Route>
-            </Routes>
-
-            <div className='container'>
-                <Hamgurger />
-            </div>
-
-        </>
-    )
+  return (
+    <div>
+        <Button/>
+    </div>
+  )
 }
-
 export default ForDisplay
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { HashRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './ArtHub_index'
+import App from './ForDisplay'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
