a5165f6161091ff624bc21d8625664d57d23ec62
chunchi1028 <wsjennt1028@gmail.com>
2025-08-18 12:55:45 +0800

    0818_行程封面做到一半

--- src/pages/Find_schedule.jsx
+++ src/pages/Find_schedule.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import'../css/all.css';
 
 const Find_schedule = () => {
   return (
-    <div>Find_schedule</div>
+    <div>
+      <div className='txt'>
+        市集 好逛，但怎麼安排才最好玩？我們規劃多種「市集＋週邊景點」的一日遊旅程
+        不只是走馬看花，更是一場有節奏的生活探索。
+        無論你是第一次逛市集，還是資深週末生活家，都可以在這裡找到屬於你的散步提案。
+      </div>
+      <div className='intro_photo'>
+        <img src="../src/images/pages/Find_schedule/intro.jpg" alt="" />
+      </div>
+    </div>
   )
 }
 
