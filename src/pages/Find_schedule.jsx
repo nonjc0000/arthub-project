ccc1d52c000365aab801abfb3c536e7932e83b81
chunchi1028 <wsjennt1028@gmail.com>
2025-08-21 16:48:12 +0800

    0821_chi

--- src/pages/Find_schedule.jsx
+++ src/pages/Find_schedule.jsx
@@ -3,23 +3,28 @@ import React from 'react'
 
 const Find_schedule = () => {
   return (
-    <section className='container'> 
-
-      <div className='titlebox'>
-        <h1>
-          <img src="../src/images/pages/Find_schedule/titlebox.svg" alt="活動行程"  style={{ width: '510px' }}/>
-        </h1>
-      </div>
-    
-      <div className='txt'>
-        不只逛市集，還有周邊的景點也可以一起安排，但怎麼安排才最好玩？我們規劃多種「市集＋週邊景點」的一日遊旅程。<br/>
-        不只是走馬看花，更是一場有節奏的生活探索。<br/>
-        無論你是第一次逛市集，還是資深週末生活家，都可以在這裡找到屬於你的散步提案。
-      </div>
-      <div className='intro_photo'>
-        <img src="../src/images/pages/Find_schedule/intro.jpg" alt="" />
-      </div>
-    </section>
+    <>
+      <section>
+        <div className='titlebox'>
+          <h1>
+            <img src="../public/images/pages/Find_schedule/titlebox.svg" alt="活動行程" style={{ width: '510px' }} />
+          </h1>
+        </div>
+        <div className='dr-box'>
+          <img src="../public/images/pages/Find_schedule/dr-box.svg" alt="裝飾線" />
+        </div>
+      </section>
+      <section>
+        <div className='txt'>
+          不只逛市集，還有周邊的景點也可以一起安排，但怎麼安排才最好玩？我們規劃多種「市集＋週邊景點」的一日遊旅程。<br />
+          不只是走馬看花，更是一場有節奏的生活探索。<br />
+          無論你是第一次逛市集，還是資深週末生活家，都可以在這裡找到屬於你的散步提案。
+        </div>
+        <div className='intro_photo'>
+          <img src="../public/images/pages/Find_schedule/intro.jpg" alt="" />
+        </div>
+      </section>
+    </>
   )
 }
 
