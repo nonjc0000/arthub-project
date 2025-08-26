7ff6f55c07a600fe74010d6fa64632f0a1b47329
PKK4823 <k4823jp@gmail.com>
2025-08-26 16:57:15 +0800

    0826寫了html、css-pkk

--- src/pages/Find_type.jsx
+++ src/pages/Find_type.jsx
@@ -9,32 +9,66 @@ const Find_type = () => {
 
     return (
         <section className='find_type_main'>
+            <h1 className='titleBox_h1'>
+                <img className='titleBox' src="../images/titlebox/find_type_titlebox.svg" /* style={{ width: '510px' }} */ alt='市集分類Market Type' />
+            </h1>
             <div className='find_type_content_box'>
-                <form className='type_search-box'>
+
+                {/* 搜尋表單 */}
+                <form className='type_search-box' /* id='type_serch' method="post" accept-charset="UTF-8" */>
+
+                    {/* 關鍵字搜尋 */}
                     <div className='search'>
-                        <input type="serch" />
-                        <img src="public\images\icon_search.svg" alt="search" />
+                        <input type="serch" id='find_type_serch' placeholder='關鍵字搜尋' />
+                        <button type="submit" value="搜尋">
+                            <img src="./images/find_map/magnifier.svg" alt="搜尋" />
+                        </button>
                     </div>
-                    <select name="find_type_city" id="find_type_city">
-                        <option value="#">選擇縣市</option>
-                    </select>
-                    <select name="find_type_area" id="find_type_area">
-                        <option value="#">選擇地區</option>
-                    </select>
-                    <p>篩選</p>
-                    <div className='find_type_btn_box'>
-                        <button>所有時間</button>
-                        <button>現在活動</button>
-                        <button>當週活動</button>
-                        <button>篩選日期</button>
+
+                    {/* 選擇縣市 */}
+                    <div className='select'>
+                        <select name="find_type_city" id="find_type_city">
+                            <option value="#">選擇縣市</option>
+                        </select>
                     </div>
-                    <select name="" id="">
-                        <option value="#">活動類型</option>
-                    </select>
+
+                    {/* 選擇地區 */}
+                    <div className='select'>
+                        <select name="find_type_area" id="find_type_area">
+                            <option value="#">選擇地區</option>
+                        </select>
+                    </div>
+
+                    {/* 按鈕篩選 */}
+                    <div>
+                        <p>篩選</p>
+                        <div className='find_type_btn_box'>
+                            <button>所有時間</button>
+                            <button>現在活動</button>
+                            <button>當週活動</button>
+                            <button>篩選日期</button>
+                        </div>
+                    </div>
+
+                    <div className='select'>
+                        <select name="" id="">
+                            <option value="#">活動類型</option>
+                        </select>
+                    </div>
                 </form>
-            </div>
-            <div className='find_type_result'>
-                <Find_type_card />
+
+                <div className='find_type_result'>
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                    <Find_type_card />
+                </div>
             </div>
         </section>
     )
