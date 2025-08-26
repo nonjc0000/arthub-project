70fc61388801af0a696e9e067c564e077fa42c2d
nonjc0000 <nonjc0000@gmail.com>
2025-08-26 22:02:42 +0800

    0826完成足跡卡片-Joy

--- src/components/My_footprint_card.jsx
+++ src/components/My_footprint_card.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const My_footprint_card = () => {
   return (
-    <div>My_footprint_card</div>
+    <div className="footprint_card">
+      <div className='date'>
+        <h2>2025</h2>
+        <p>6/20 FRI</p>
+        <p>|</p>
+        <p>6/22 SUN</p>
+      </div>
+      <div className='location'>
+        <h2>《夏日派對》</h2>
+        <p>大稻埕碼頭貨櫃市集</p>
+      </div>
+      <figure className='footprint_img'>
+        <img src="./images/My_footprint/market.jpg" alt="" />
+      </figure>
+    </div>
   )
 }
 
--- src/pages/My_footprint.jsx
+++ src/pages/My_footprint.jsx
@@ -2,58 +2,58 @@ import React from 'react'
 import My_footprint_card from '../components/My_footprint_card'
 
 const My_footprint = () => {
-  return (
-    <section className='my_footprint_container'>
-        <h1 className='titleBox_h1'>
-          <img className='titleBox' src="../images/titlebox/my_footprint_titlebox.svg" alt='我的足跡My Footprints' />
-        </h1>
+    return (
+        <section className='my_footprint_container'>
+            <h1 className='titleBox_h1'>
+                <img className='titleBox' src="../images/titlebox/my_footprint_titlebox.svg" alt='我的足跡My Footprints' />
+            </h1>
 
-    <div className="hr_square_deco">
-        
-    </div>
+            <div className="hr_square_deco">
 
-<section className="footprint_content_box">
+            </div>
 
-    <aside className="user_sidebar">
-        <div className='sidebar_item'>
-            <button></button>
-            <p>我的足跡</p>
-        </div>
-        <div className='sidebar_item'>
-            <button></button>
-            <p>訊息通知</p>
-        </div>
-        <div className='sidebar_item'>
-            <button></button>
-            <p>訂單管理</p>
-        </div>
+            <section className="footprint_content_box">
 
-    </aside>
+                <aside className="user_sidebar">
+                    <div className='sidebar_item'>
+                        <button></button>
+                        <p>我的足跡</p>
+                    </div>
+                    <div className='sidebar_item'>
+                        <button></button>
+                        <p>訊息通知</p>
+                    </div>
+                    <div className='sidebar_item'>
+                        <button></button>
+                        <p>訂單管理</p>
+                    </div>
 
-    <div className="footprint_content">
-        <div className='.footprint_content_topbar'></div>
+                </aside>
 
-        <div className='.footprint_result'>
-            <My_footprint_card/>
-            <My_footprint_card/>
-            <My_footprint_card/>
-            <My_footprint_card/>
-            <My_footprint_card/>
-        </div>
-    </div>
+                <div className="footprint_content">
+                    <div className='footprint_content_topbar'></div>
 
-    <aside className="user_sidebar">
-        <svg></svg>
-    </aside>
-</section>
+                    <div className='footprint_result'>
+                        <My_footprint_card />
+                        <My_footprint_card />
+                        <My_footprint_card />
+                        <My_footprint_card />
+                        <My_footprint_card />
+                    </div>
+                </div>
+
+                <aside className="user_sidebar">
+                    <svg></svg>
+                </aside>
+            </section>
+            <figure className="map_deco1">
+
+            </figure>
 
 
 
-
-
-
-    </section>
-  )
+        </section>
+    )
 }
 
 export default My_footprint
