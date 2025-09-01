5e0cb70150aa463d1ec8eaed1ba17b0cebfbf610
nonjc0000 <nonjc0000@gmail.com>
2025-09-01 09:03:48 +0800

    0901 hamburger-Joy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,94 @@
 import { Link } from 'react-router-dom'
+import $ from 'jquery'
+import { useEffect } from 'react';
 
 const NavBar = () => {
+    useEffect(() => {
+        // 漢堡按鈕
+        const handler = function () {
+            $(this).toggleClass('is-active');
+            $('.navigation').toggleClass('show');
+        };
+
+        const $btn = $('.hamburger');
+        // 先解除同一個 handler，避免重複綁定
+        $btn.off('click', handler).on('click', handler);
+
+        // 清理（StrictMode 下第二次 mount/unmount 也不會殘留）
+        return () => {
+            $btn.off('click', handler);
+        };
+    }, []);
+
     return (
-        <div>
-            <ul>
-                <li> <Link to="/Find_type">找類型</Link></li>
-                <li> <Link to="/Find_map">找地圖</Link></li>
-                <li><Link to="/Find_schedule">找行程</Link></li>
-                <li><Link to="/Blog">部落格</Link></li>
-                <li><Link to="/About">關於我們</Link></li>
-                <li><Link to="/My_footprint">會員登入</Link></li>
-            </ul>
+        <div className='topbar'>
+            {/* logo */}
+            <div className="logo">
+                <img src="./images/logo.svg" alt="Logo" />
+            </div>
+
+            <div className='navbar'>
+                {/* 漢堡按鈕 */}
+                <button className="hamburger">
+                    <span className="bar" style={{ width: '80px' }}></span>
+                    <span className="bar" style={{ width: '70px' }}></span>
+                    <span className="bar" style={{ width: '60px' }}></span>
+                </button>
+
+                <nav className='navigation'>
+                    <ul>
+                        <li className='find_type'>
+                            <Link to="/Find_type">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/find_type-accent.svg" style={{ width: '24px' }} />
+                                    找類型
+                                </p>
+                            </Link>
+                        </li>
+                        <li className='find_map'>
+                            <Link to="/Find_map">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/find_map-accent.svg" style={{ width: '24px' }} />
+                                    找地圖
+                                </p>
+                            </Link>
+                        </li>
+                        <li className='find_schedule'>
+
+                            <Link to="/Find_schedule">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/find_schedule-accent.svg" style={{ width: '24px' }} />
+                                    找行程
+                                </p>
+                            </Link>
+                        </li>
+                        <li className='blog'>
+                            <Link to="/Blog">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/blog-accent.svg" style={{ width: '24px' }} />
+                                    部落格
+                                </p>
+                            </Link>
+                        </li>
+                        <li className='about'>
+                            <Link to="/About">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/about-accent.svg" style={{ width: '24px' }} />
+                                    關於我們
+                                </p>
+                            </Link>
+                        </li>
+                        <li className='user'>
+                            <Link to="/My_footprint">
+                                <p className='sitemap_item_content'>
+                                    <img src="./images/icons/user-accent.svg" style={{ width: '24px' }} />
+                                    會員登入
+                                </p>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div >
     )
 }
