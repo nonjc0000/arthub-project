3979b99ddccd3f36ab7053da3bfcb3f9518fd2aa
nonjc0000 <nonjc0000@gmail.com>
2025-09-03 14:32:29 +0800

    0903-Joy

--- src/components/PageTop.jsx
+++ src/components/PageTop.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const GoTop = () => {
+const PageTop = () => {
   return (
     <div className='pageTop_box'>
       <a href="#" className='pageTop_link'>
       <div className='pageTop_text'>
-        <img src="src/images/pageTop_text.svg" alt="" />
+        <img src="./images/pageTop/pageTop_text.svg" alt="" />
       </div>
       <div className='pageTop_finger'>
-        <img src="src/images/pageTop_finger.svg" alt="" />
+        <img src="./images/pageTop/pageTop_finger.svg" alt="" />
       </div>
       </a>
     </div>
   )
 }
 
-export default GoTop
+export default PageTop
--- src/pages/Order_management.jsx
+++ src/pages/Order_management.jsx
@@ -21,17 +21,39 @@ const Order_management = () => {
                     <div className='order_actions'>
                         <button className='btn_pass'>
                             <div className='btn_content'>
-                            <img src="./images/order_management/pass_icon.svg" alt=""/>
-                            <span>查看憑證</span>
+                                <img src="./images/order_management/pass_icon.svg" alt="" />
+                                <span>查看憑證</span>
                             </div>
-                            </button>
+                        </button>
                         <button className='btn_comment'>
                             <div className='btn_content'>
-                            <img src="./images/order_management/comment_icon.svg" alt=""/>
-                            <span>評論</span>
+                                <img src="./images/order_management/comment_icon.svg" alt="" />
+                                <span>評論</span>
                             </div>
                         </button>
                     </div>
+                </div>
+            </div>
+        )
+    }
+
+    // 推薦卡片
+    const Recommended_card = () => {
+        return (
+            <div className='recommended_card'>
+                <img src="./images/order_management/recommended1.png" alt="大稻埕貓路微章" />
+                <div className='recommended_info'>
+                    <h4>大稻埕風格微旅</h4>
+                    <div className='recommended_tags'>
+                        <span className='tag'>藝文展場</span>
+                        <span className='tag'>小型市集</span>
+                        <span className='tag'>河岸夕陽</span>
+                    </div>
+                    <div className='recommended_rating'>
+                        <span className='stars'>★★★★★</span>
+                        <span className='rating_text'>4.8(93) | 200人參加過</span>
+                    </div>
+                    <p className='price'>NT $800元起</p>
                 </div>
             </div>
         )
@@ -78,39 +100,8 @@ const Order_management = () => {
                         <div className='recommended_section'>
                             <h3>屬於你的市集靈感</h3>
 
-                            <div className='recommended_card'>
-                                <img src="./images/order_management/recommended1.png" alt="大稻埕貓路微章" />
-                                <div className='recommended_info'>
-                                    <h4>大稻埕風格微旅</h4>
-                                    <div className='recommended_tags'>
-                                        <span className='tag'>藝文展場</span>
-                                        <span className='tag'>小型市集</span>
-                                        <span className='tag'>河岸夕陽</span>
-                                    </div>
-                                    <div className='recommended_rating'>
-                                        <span className='stars'>★★★★★</span>
-                                        <span className='rating_text'>4.8(93) | 200人參加過</span>
-                                    </div>
-                                    <p className='price'>NT $800元起</p>
-                                </div>
-                            </div>
-
-                            <div className='recommended_card'>
-                                <img src="./images/order_management/recommended1.png" alt="大稻埕貓路微章" />
-                                <div className='recommended_info'>
-                                    <h4>大稻埕風格微旅</h4>
-                                    <div className='recommended_tags'>
-                                        <span className='tag'>藝文展場</span>
-                                        <span className='tag'>小型市集</span>
-                                        <span className='tag'>河岸夕陽</span>
-                                    </div>
-                                    <div className='recommended_rating'>
-                                        <span className='stars'>★★★★★</span>
-                                        <span className='rating_text'>4.8(93) | 200人參加過</span>
-                                    </div>
-                                    <p className='price'>NT $800元起</p>
-                                </div>
-                            </div>
+                            <Recommended_card />
+                            <Recommended_card />
                         </div>
                     </div>
                 </div>
