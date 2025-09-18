cfc6ddd5f3fe9efa0c550ca6f28a1204dd96d631
nonjc0000 <nonjc0000@gmail.com>
2025-09-18 15:05:07 +0800

    0918 all git - Joy

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -2,33 +2,38 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
+
+
 const HomeCard = () => {
-    const arrCard=[
+    const arrCard = [
         {
-            id:1,
-            image:"./images/home/home-card.png",
-            time:"2025.6.15",
-            name:"某某某",
-            title1:"城市裡的一場迷你旅行：",
-            title2:"我在市集中發現的生活靈感"
+            id: 1,
+            image: "./images/home/home-card.png",
+            time: "2025.6.15",
+            name: "某某某",
+            title1: "城市裡的一場迷你旅行：",
+            title2: "我在市集中發現的生活靈感",
+            slug: ""
 
 
         },
         {
-            id:2,
-            image:"./images/home/home-card.png",
-            time:"2025.6.15",
-            name:"某某某",
-            title1:"城市裡的一場迷你旅行：",
-            title2:"我在市集中發現的生活靈感"
+            id: 2,
+            image: "./images/home/home-card.png",
+            time: "2025.6.15",
+            name: "某某某",
+            title1: "城市裡的一場迷你旅行：",
+            title2: "我在市集中發現的生活靈感",
+            slug: ""
         },
         {
-            id:3,
-            image:"./images/home/home-card.png",
-            time:"2025.6.15",
-            name:"某某某",
-            title1:"城市裡的一場迷你旅行：",
-            title2:"我在市集中發現的生活靈感"
+            id: 3,
+            image: "./images/home/home-card.png",
+            time: "2025.6.15",
+            name: "某某某",
+            title1: "城市裡的一場迷你旅行：",
+            title2: "我在市集中發現的生活靈感",
+            slug: ""
         }
     ]
     return (
@@ -36,27 +41,34 @@ const HomeCard = () => {
             {/* 卡片 */}
 
 
-            <Link to="">
-                {arrCard.map(blog => (
-                    <div key={blog.id} className='home-card'>
-                        <div>
-                            <img src={blog.image} alt="home-card" />
-                        </div>
-
-
-                        <div className='home-card-item'>
-                            <p>{blog.time}<span>{blog.name}</span></p>
-                            <div className='home-card-p'>
-                                <p>{blog.title1}<br />{blog.title2}</p>
+            <div className='home-cards-container'>
+               
+                    {arrCard.map(blog => (
+                        <Link key={blog.id} className="home-card-link" to={`/blog/${blog.slug}`}>
+                        <div className='home-card'>
+                            <div>
+                                <img src={blog.image} alt="home-card" />
+                            </div>
+                            <div className='home-card-item'>
+                                <p>{blog.time}<span>{blog.name}</span></p>
+                                <div className='home-card-p'>
+                                    <p>{blog.title1}<br />{blog.title2}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </Link>
+                        </Link>
+                    ))}
+            </div>
         </>
     )
 }
 
 
+
+
 export default HomeCard
 
+
+
+
+  
--- src/pages/Find_schedule.jsx
+++ src/pages/Find_schedule.jsx
@@ -1,5 +1,7 @@
 import Region_card from "../components/Find_schedule/Region_card";
-import ScrollToTop from '../components/ScrollToTop'
+
+
+
 
 const arrCards = [
   {
@@ -28,15 +30,19 @@ const arrCards = [
   },
 ];
 
+
+
+
 const Find_schedule = () => {
   return (
     <>
-    <ScrollToTop/>
       <div className="container">
+
+
         <div className="find_schedule_titlebox">
           <h1 className="titleBox_h1">
             <img
-            className="titleblx"
+              className="titlebox"
               src="./images/titlebox/find_schedule_titlebox.svg"
               alt="活動行程"
             />
@@ -45,6 +51,9 @@ const Find_schedule = () => {
         <div className="deco_line">
           <img src="./images/Find_schedule/deco_line.svg" alt="" />
         </div>
+
+
+
 
         <div className="region">
           {arrCards.map((region) => {
@@ -59,23 +68,52 @@ const Find_schedule = () => {
           })}
         </div>
 
+
+
+
         <div className="intro_down">
-          <div className="txt">
-            不只逛市集，還有周邊的景點也可以一起安排，但怎麼安排才最好玩？
-            <br />
-            我們規劃多種「市集＋週邊景點」的一日遊旅程。
-            <br />
-            不只是走馬看花，更是一場有節奏的生活探索。
-            <br />
-            無論你是第一次逛市集，還是資深週末生活家，都可以在這裡找到屬於你的散步提案。
+          <div className="txt-background">
+            <div className="deco-girl">
+              <img src="./images/Find_schedule/girl.svg" alt="" />
+            </div>
+            <div className="deco-cord">
+              <img src="./images/Find_schedule/normal-cord.svg" alt="" />
+            </div>
+            <div className="txt">
+              不只逛市集，還有周邊的景點也可以一起安排，但怎麼安排才最好玩？
+              <br />
+              我們規劃多種「市集＋週邊景點」的一日遊旅程。
+              <br />
+              不只是走馬看花，更是一場有節奏的生活探索。
+              <br />
+              無論你是第一次逛市集，還是資深週末生活家，都可以在這裡找到屬於你的散步提案。
+            </div>
           </div>
           <div className="intro_photo">
+            <div className="deco-getFun">
+              <img src="./images/Find_schedule/getFun.svg" alt="" />
+            </div>
             <img src="./images/Find_schedule/intro.png" alt="" />
+           
+              <div className="road">
+                <img className="car-01"src="./images/Find_schedule/car01.svg" alt="" />
+              </div>
+              <div className="car-go">
+                <img src="./images/Find_schedule/car-go.svg" alt="" />
+            </div>
           </div>
+
+
+
+
         </div>
       </div>
     </>
   );
 };
 
+
+
+
 export default Find_schedule;
+
